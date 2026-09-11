ec6cb14f83493291d59d47aa34e9bd7f05f91c20
cristian64 <cristian64@gmail.com>
2026-09-12 00:49:27 +0100

    PrintProgressive: Add class implementation.

--- include/Kameda/PrintProgressive.h
+++ include/Kameda/PrintProgressive.h
@@ -1,13 +1,18 @@
 #ifndef PRINTPROGRESSIVE_H
 #define PRINTPROGRESSIVE_H
 
-#include <JSystem/JKernel/JKRHeap.h>
+#include "JSystem/J2D/J2DGrafContext.h"
+#include "JSystem/J2D/J2DPane.h"
+#include "JSystem/J2D/J2DPicture.h"
+#include "JSystem/J2D/J2DScreen.h"
+#include "JSystem/JKernel/JKRArchive.h"
+#include "JSystem/JKernel/JKRHeap.h"
 #include "types.h"
 
 class PrintProgressive
 {
 public:
-    enum MessageID // according to debug test screen
+    enum MessageID  // according to debug test screen
     {
         mcShow_Dialog,
         mcSet_Option1,
@@ -15,29 +20,54 @@ public:
         mcShow_Logo,
         mcShow_Set
     };
-    PrintProgressive(JKRHeap *); // 0x8016aac4
-    void init(MessageID);        // 0x8016b138
-    void draw();                 // 0x8016b65c
-    void calc(int);              // 0x8016b6dc
-    void isMessage();            // 0x8016bc28
-    void isChoice();             // 0x8016bcbc
-    void getCounter();           // 0x8016bd48
+
+    PrintProgressive(JKRHeap *heap);  // 0x8016aac4
+    void init(MessageID);             // 0x8016b138
+    void draw();                      // 0x8016b65c
+    void calc(int);                   // 0x8016b6dc
+    bool isMessage();                 // 0x8016bc28
+    bool isChoice();                  // 0x8016bcbc
+    s32 getCounter();                 // 0x8016bd48
     // Inline/Unused
     ~PrintProgressive();
     void calc_bck();
     void calc_texChange();
     void setCounter(int);
 
-    int getState() { return _68; }
-    int getOption() { return _6c; }
+    int getState() { return mState; }
+    int getOption() { return mOption; }
 
 private:
-    // TODO
-    u8 _0[0x64];
-    MessageID mMsgId;
-    int _68;
-    int _6c;
-    u8 _70[0x94 -0x70];
-}; // Size: 0x94
+    void *mColorRes;                   // 0x0
+    void *mProgressiveScanRes;         // 0x4
+    void *mYesNoRes;                   // 0x8
+    J2DOrthoGraph *mOrthoGraph;        // 0xc
+    JKRArchive *mArchive;              // 0x10
+    J2DScreen *mScreenLogo;            // 0x14
+    J2DScreen *mScreenProgressive;     // 0x18
+    J2DPicture *mPane;                 // 0x1c
+    J2DTextBox *mTextBox;              // 0x20
+    J2DTextBox *mTextBox2;             // 0x24
+    J2DTextBox *mTextBox3;             // 0x28
+    J2DTextBox *mTextBox4;             // 0x2c
+    J2DPicture *mPane2;                // 0x30
+    J2DPicture *mPane3;                // 0x34
+    J2DPicture *mPane4;                // 0x38
+    J2DPicture *mPane5;                // 0x3c
+    J2DAnmTransform *mTransform;       // 0x40
+    J2DAnmColor *mColors[4];           // 0x44
+    J2DAnmTextureSRTKey *mSTRKey;      // 0x54
+    u8 _58[4];                         // 0x58
+    u8 _5c[4];                         // 0x5c
+    s32 _60;                           // 0x60
+    MessageID mMsgId;                  // 0x64
+    int mState;                        // 0x68
+    int mOption;                       // 0x6c
+    u32 _70;                           // 0x70
+    J2DTextBox::TFontSize mFontSize;   // 0x74
+    J2DTextBox::TFontSize mFontSize2;  // 0x7c
+    J2DTextBox::TFontSize mFontSize3;  // 0x84
+    J2DTextBox::TFontSize mFontSize4;  // 0x8c
+};  // Size: 0x94
 
-#endif // PRINTPROGRESSIVE_H
+#endif  // PRINTPROGRESSIVE_H
--- src/Kameda/PrintProgressive.cpp
+++ src/Kameda/PrintProgressive.cpp
@@ -1,17 +1,209 @@
 #include "Kameda/PrintProgressive.h"
+#include "JSystem/J2D/J2DAnmLoader.h"
+#include "JSystem/J2D/J2DPicture.h"
+#include "JSystem/JKernel/JKRArchive.h"
+#include "JSystem/JKernel/JKRFileLoader.h"
+#include "JSystem/JUtility/JUTAssert.h"
+#include "Kameda/ReadPrintMessage.h"
+#include "Osako/FontMgr.h"
+#include "Osako/ResMgr.h"
+#include "Osako/system.h"
 
-PrintProgressive::PrintProgressive(JKRHeap *) {}
+PrintProgressive::PrintProgressive(JKRHeap *heap)
+{
+    mOrthoGraph = System::getJ2DOrtho();
+
+    mArchive = JKRMountArchive(ResMgr::getPtr(ResMgr::mcArcSystem, "Screen/progressive.arc"),
+                               heap,
+                               JKRArchive::MOUNT_DIRECTION_HEAD);
+
+    mScreenLogo = new (heap, 0) J2DScreen();
+    mScreenLogo->J2DScreen::set("NintendoLogo.blo", 0x40000, mArchive);
+
+    mScreenProgressive = new (heap, 0) J2DScreen();
+    mScreenProgressive->J2DScreen::set("progressive.blo", 0x40000, mArchive);
+
+    mTransform = (J2DAnmTransform *)J2DAnmLoaderDataBase::load(
+        JKRFileLoader::getGlbResource("Progressive.bck", mArchive));
+    mScreenProgressive->setAnimation(mTransform);
+
+    {
+        J2DPicture *pane = (J2DPicture *)mScreenProgressive->search('n_an');
+#line 49
+        JUT_ASSERT(pane->getTypeID() == J2DPane_Picture);
+        mPane = pane;
+    }
+
+    mTextBox = (J2DTextBox *)mScreenProgressive->search('T_tx1');
+    mTextBox->setFont(FontMgr::mspResFont);
+    mTextBox->getFontSize(mFontSize);
+
+    mTextBox2 = (J2DTextBox *)mScreenProgressive->search('T_yes');
+    mTextBox2->setFont(FontMgr::mspResFont);
+    mTextBox2->getFontSize(mFontSize2);
+
+    mTextBox3 = (J2DTextBox *)mScreenProgressive->search('T_no');
+    mTextBox3->setFont(FontMgr::mspResFont);
+    mTextBox3->getFontSize(mFontSize3);
+
+    mTextBox4 = (J2DTextBox *)mScreenProgressive->search('Num1');
+    mTextBox4->setFont(FontMgr::mspResFont);
+
+    {
+        J2DPicture *pane = (J2DPicture *)mScreenProgressive->search('lyes');
+#line 66
+        JUT_ASSERT(pane->getTypeID() == J2DPane_Picture);
+        mPane2 = pane;
+    }
+    mPane2->setAnimation(mTransform);
+
+    {
+        J2DPicture *pane = (J2DPicture *)mScreenProgressive->search('lno');
+#line 72
+        JUT_ASSERT(pane->getTypeID() == J2DPane_Picture);
+        mPane3 = pane;
+    }
+    mPane3->setAnimation(mTransform);
+
+    {
+        J2DPicture *pane = (J2DPicture *)mScreenLogo->search('m_nin1');
+#line 78
+        JUT_ASSERT(pane->getTypeID() == J2DPane_Picture);
+        mPane4 = pane;
+    }
+
+    {
+        J2DPicture *pane = (J2DPicture *)mScreenLogo->search('Dlby0');
+#line 83
+        JUT_ASSERT(pane->getTypeID() == J2DPane_Picture);
+        mPane5 = pane;
+    }
+
+    void *resource = JKRGetNameResource("Progressive.bpk", mArchive);
+    for (int i = 0; i < 4; ++i)
+    {
+        mColors[i] = (J2DAnmColor *)J2DAnmLoaderDataBase::load(resource);
+        mColors[i]->searchUpdateMaterialID(mScreenProgressive);
+    }
+
+    mTextBox2->setAnimation(mColors[0]);
+    mTextBox3->setAnimation(mColors[1]);
+    mPane2->setAnimation(mColors[2]);
+    mPane3->setAnimation(mColors[3]);
+
+    mSTRKey = (J2DAnmTextureSRTKey *)J2DAnmLoaderDataBase::load(
+        JKRFileLoader::getGlbResource("Progressive.btk", mArchive));
+    mScreenProgressive->setAnimation(mSTRKey);
+
+    mColorRes = ResMgr::getPtr(ResMgr::mcColor);
+    mProgressiveScanRes = ResMgr::getPtr(ResMgr::mcProgressiveScan);
+    mYesNoRes = ResMgr::getPtr(ResMgr::mcYesNo);
+
+    JUTColor color =
+        ReadPrintMessage::getColor(mColorRes,
+                                   ReadPrintMessage::getDefaultColor(mProgressiveScanRes));
+    mTextBox->setFontColor(color, color);
+
+    char buffer[512];
+    for (int i = 0; i < 512; ++i)
+    {
+        buffer[i] = ' ';
+    }
+    buffer[511] = '\0';
+    mTextBox->setString(buffer);
+    buffer[15] = '\0';
+    mTextBox2->setString(buffer);
+    mTextBox3->setString(buffer);
+    buffer[1] = '\0';
+    mTextBox4->setString(buffer);
+}
 
 void PrintProgressive::init(PrintProgressive::MessageID) {}
 
-void PrintProgressive::draw() {}
+void PrintProgressive::draw()
+{
+    mOrthoGraph->setPort();
+
+    switch (mMsgId)
+    {
+    case mcShow_Logo:
+    case mcShow_Set:
+        mScreenLogo->draw(0.0f, 0.0f, mOrthoGraph);
+        break;
+    default:
+        mScreenProgressive->draw(0.0f, 0.0f, mOrthoGraph);
+    }
+}
 
 void PrintProgressive::calc(int) {}
 
-void PrintProgressive::isMessage() {}
+bool PrintProgressive::isMessage()
+{
+    bool result;
 
-void PrintProgressive::isChoice() {}
+    switch (mMsgId)
+    {
+    case mcShow_Dialog:
+    case mcSet_Option1:
+    case mcSet_Option2:
+        result = true;
+        break;
+    case mcShow_Logo:
+    case mcShow_Set:
+        result = false;
+        break;
+    default:
+#line 497
+        JUT_ASSERT(0);
+    }
 
-void PrintProgressive::getCounter() {}
+    return result;
+}
 
-#include "JSystem/JAudio/JASFakeMatch.h"
+bool PrintProgressive::isChoice()
+{
+    bool result;
+
+    switch (mMsgId)
+    {
+    case mcShow_Dialog:
+        result = true;
+        break;
+    case mcSet_Option1:
+    case mcSet_Option2:
+    case mcShow_Logo:
+    case mcShow_Set:
+        result = false;
+        break;
+    default:
+#line 524
+        JUT_ASSERT(0);
+    }
+
+    return result;
+}
+
+s32 PrintProgressive::getCounter()
+{
+    s32 result = -1;
+    switch (mMsgId)
+    {
+    case mcShow_Dialog:
+        if (_60 != 0)
+        {
+            result = _60 / 0x3c + 1;
+        }
+        else
+        {
+
+            result = 0;
+        }
+        break;
+    default:
+        break;
+    }
+
+    return result;
+}
+
+#include "JSystem/JAudio/JASFakeMatch.h"  // For static initializer
